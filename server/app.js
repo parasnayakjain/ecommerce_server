3889cdf1e80d5c67a9c2ad58047027b584cf17f0
parasnayakjain <73302962+parasnayakjain@users.noreply.github.com>
2022-07-03 13:12:33 +0530

    avatar

--- server/app.js
+++ server/app.js
@@ -4,12 +4,18 @@ const app=express();
 const product=require("./routes/products")
 const user=require("./routes/user");
 const order=require("./routes/order");
+const bodyParser=require("body-parser");
+const fileUpload=require("express-fileupload");
 const connectDB=require("./database/connect");
 const errorHandler = require("./middleware/error");
 const cors=require("cors");
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload());
+
+
 app.use("/api/v1",product);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
