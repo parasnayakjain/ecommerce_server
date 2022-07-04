792db0cb00699f4323e6973c0929cdea1bcd6620
parasnayakjain <73302962+parasnayakjain@users.noreply.github.com>
2022-07-04 18:03:39 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,6 +19,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 
 app.use("/api/v1",product);
