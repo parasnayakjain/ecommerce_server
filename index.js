a50629a00830f8c7622a16d551b6feae58aaa4f0
parasnayakjain <73302962+parasnayakjain@users.noreply.github.com>
2022-11-23 22:23:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = require("./server/app.js");;
 const PORT = process.env.PORT ;
-
+const cloudinary=require("cloudinary");
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
